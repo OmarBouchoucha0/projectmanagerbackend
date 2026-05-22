e043ab9f26789eef5f7802f6816514d2f25ab872
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-05-22 14:51:20 +0100

    feat: adding depandecies and hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, Axum!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
+
+    println!("Listening on http://localhost:3000");
+
+    axum::serve(listener, app).await.unwrap();
 }
