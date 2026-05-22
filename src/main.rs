227fdbbb356fd1cbfc9c82569e723255d1130ae9
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-05-22 15:00:55 +0100

    feat: cors

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use axum::{Router, routing::get};
+use tower_http::cors::CorsLayer;
+
+async fn test() -> &'static str {
+    "Hello from Backend!"
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, Axum!" }));
+    let app = Router::new()
+        .route("/api/test", get(test))
+        .layer(CorsLayer::permissive());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
 
-    println!("Listening on http://localhost:3000");
+    println!("Listening on http://localhost:5000");
 
     axum::serve(listener, app).await.unwrap();
 }
