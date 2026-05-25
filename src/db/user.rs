a3b502b370fcb42d1ab1b1276cb716dd21bd24f6
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-05-25 16:05:33 +0100

    feat: user exists db call

--- src/db/user.rs
+++ src/db/user.rs
@@ -1,11 +1,20 @@
-use sqlx::{Pool, Sqlite, sqlite::SqliteQueryResult};
+use sqlx::{FromRow, Pool, Sqlite, sqlite::SqliteQueryResult};
+
+#[derive(FromRow)]
+pub struct User {
+    id: u32,
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String,
+}
 
 pub async fn user_init(pool: &Pool<Sqlite>) -> Result<SqliteQueryResult, sqlx::Error> {
     sqlx::query(
         r#"
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
+            firstname TEXT NOT NULL,
             lastname TEXT NOT NULL,
             password TEXT NOT NULL,
             email TEXT NOT NULL
@@ -18,13 +27,13 @@ pub async fn user_init(pool: &Pool<Sqlite>) -> Result<SqliteQueryResult, sqlx::E
 
 pub async fn user_create(
     pool: &Pool<Sqlite>,
-    name: &str,
+    firstname: &str,
     lastname: &str,
     email: &str,
     password: &str,
 ) -> Result<SqliteQueryResult, sqlx::Error> {
-    sqlx::query("INSERT INTO users (name,email,password) VALUES (?,?,?)")
-        .bind(name)
+    sqlx::query("INSERT INTO users (firstname,lastname,email,password) VALUES (?,?,?,?)")
+        .bind(firstname)
         .bind(lastname)
         .bind(email)
         .bind(password)
@@ -34,7 +43,7 @@ pub async fn user_create(
 
 pub async fn user_update(
     pool: &Pool<Sqlite>,
-    name: &str,
+    firstname: &str,
     lastname: &str,
     email: &str,
     password: &str,
@@ -42,14 +51,31 @@ pub async fn user_update(
     sqlx::query(
         r#"
         UPDATE users 
-        SET name = ?, lastname = ?, password = ? 
+        SET firstname = ?, lastname = ?, password = ? 
         WHERE email = ?
         "#,
     )
-    .bind(name)
+    .bind(firstname)
     .bind(lastname)
     .bind(email)
     .bind(password)
     .execute(pool)
+    .await
+}
+
+pub async fn user_exists(
+    pool: &Pool<Sqlite>,
+    email: &str,
+    password: &str,
+) -> Result<Option<User>, sqlx::Error> {
+    sqlx::query_as::<_, User>(
+        r#"
+        SELECT * from users 
+        WHERE email = ? and password = ?
+        "#,
+    )
+    .bind(email)
+    .bind(password)
+    .fetch_optional(pool)
     .await
 }
