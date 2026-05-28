1c86a61e1c34398e721c7b61a303a503069422b3
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-05-28 01:49:21 +0100

    feat: db init update

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,12 @@
 pub mod init;
+pub mod project;
 pub mod user;
+pub mod workspace;
 use anyhow::{Context, Result};
-use sqlx::{Pool, Sqlite, sqlite::SqliteQueryResult};
+use sqlx::{Pool, Sqlite};
 
 use crate::db::init::init_db;
 
-pub async fn db(pool: &Pool<Sqlite>) -> Result<SqliteQueryResult> {
+pub async fn db(pool: &Pool<Sqlite>) -> Result<()> {
     init_db(pool).await.context("failed to initilize db")
 }
--- src/db/init.rs
+++ src/db/init.rs
@@ -1,7 +1,10 @@
-use sqlx::{Pool, Sqlite, sqlite::SqliteQueryResult};
+use sqlx::{Pool, Sqlite};
 
-use crate::db::user::user_init;
+use crate::db::{project::project_init, user::user_init, workspace::workspace_init};
 
-pub async fn init_db(pool: &Pool<Sqlite>) -> Result<SqliteQueryResult, sqlx::Error> {
-    user_init(pool).await
+pub async fn init_db(pool: &Pool<Sqlite>) -> Result<(), sqlx::Error> {
+    user_init(pool).await?;
+    workspace_init(pool).await?;
+    project_init(pool).await?;
+    Ok(())
 }
